d507f321cef93d2b9cbfd66e0ded5a0a0e7ddc4d
farhan <el3317.farhanmahmood@gmail.com>
2024-07-30 23:51:03 +0530

    added pagination files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Pagination from './Components/Pagination'
+const App = () => {
+  return (
+    <>
+      <Pagination/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Pagination.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import "./Pagination.css";
+const Pagination = () => {
+  const [fetchedData, setFetchedData] = useState("");
+  const [isDataFetched, setIsDataFetched] = useState(false);
+  const [currentIndex,setCurrentIndex]=useState(0);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const FetchData = async () => {
+      try {
+        const responseObject = await fetch("https://fakestoreapi.com/products");
+        const javaScriptObject = await responseObject.json();
+        setFetchedData(javaScriptObject);
+      } catch (err) {
+        console.log("error fetching data", err);
+      } finally {
+        setIsDataFetched(true);
+      }
+    };
+    FetchData();
+  }, []);
+
+  const disablePrevious=currentIndex===0;
+  const disableNext=currentIndex===fetchedData.length/4-1;
+
+  const handleClick = (id) => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    setCurrentIndex(id-1);
+    setPage(id);
+    
+  };
+
+  return (
+    <>
+      <div className="MainContainer">
+        {isDataFetched ? (
+          fetchedData.slice(page * 4 - 4, page * 4).map((val, id) => (
+            <div key={id} className="FetchedContainer">
+              <h3>
+                {val.title}
+              </h3>
+              <img src={val.image} />
+            </div>
+          ))
+        ) : (
+          <p>Loading....</p>
+        )}
+      </div>
+      {isDataFetched && (
+        <div class="paginationButtons">
+          <button onClick={()=> handleClick(page-1)} disabled={disablePrevious}>◀️</button>
+          {[...Array(fetchedData.length / 4)].map((_, id) => {
+            return (
+              <button className={page == id+1 ? "selectedPage":""} onClick={() => handleClick(id+1)} key="{id}">
+                {id+1}
+              </button>
+            );
+          })}
+          <button onClick={()=> handleClick(page+1)} disabled={disableNext}>▶️</button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Pagination;
